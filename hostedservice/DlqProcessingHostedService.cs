77a07afe63d7a8c44f48dd9706253aa5b5bfc057
Tim Hess <thess@pivotal.io>
2018-11-09 11:12:37 -0600

    add a sample

---
+++ hostedservice/DlqProcessingHostedService.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Steeltoe.Management.Endpoint.Health;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace hostedservice
+{
+    public class DlqProcessingHostedService : IHostedService
+    {
+        private readonly IApplicationLifetime _appLifetime;
+
+        private readonly ILogger<DlqProcessingHostedService> _logger;
+
+        private readonly IServiceScopeFactory _serviceScopeFactory;
+
+        public DlqProcessingHostedService(IApplicationLifetime appLifetime,
+            IServiceScopeFactory serviceScopeFactory,
+            ILogger<DlqProcessingHostedService> logger)
+        {
+            _appLifetime = appLifetime;
+            _logger = logger;
+            _serviceScopeFactory = serviceScopeFactory;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            using (var scope = _serviceScopeFactory.CreateScope())
+            {
+                var he = scope.ServiceProvider.GetRequiredService<HealthEndpoint>();
+                //var worker = scope.ServiceProvider.GetRequiredService<IWorker>();
+                //CheckStartupHealth(he);
+                //await worker.ProcessDlxMessages();
+                Console.WriteLine("Waiting for a thing...");
+                await Task.Delay(5000);
+                Console.WriteLine("Done waiting for a thing.");
+            }
+            _appLifetime.StopApplication();
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Finished executing task!");
+        }
+    }
+}
---
+++ hostedservice/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Steeltoe.Extensions.Configuration.CloudFoundry;
+using Steeltoe.Extensions.Logging;
+
+namespace hostedservice
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //var host = new HostBuilder()
+            //.ConfigureAppConfiguration((hostContext, config) =>
+            //{
+            //    var env = hostContext.HostingEnvironment;
+            //    config.SetBasePath(Directory.GetCurrentDirectory());
+            //    config.AddEnvironmentVariables();
+            //    config.AddCommandLine(args);
+            //    config.AddJsonFile("appsettings.json", true, false);
+            //    config.AddJsonFile($"appsettings.{env.EnvironmentName}.json", true, false);
+            //    config.AddCloudFoundry();
+            //})
+            //.ConfigureServices((hostContext, services) =>
+            //{
+            //    ConfigureServices(services, hostContext.Configuration);
+            //})
+            //.ConfigureLogging((hostContext, logBuilder) =>
+            //{
+            //    logBuilder.ClearProviders();
+            //    logBuilder.AddConfiguration(hostContext.Configuration.GetSection("Logging"));
+            //    logBuilder.AddDynamicConsole();
+            //    if (hostContext.HostingEnvironment.IsDevelopment())
+            //    {
+            //        logBuilder.AddDebug();
+            //    }
+            //});
+            //await host.RunConsoleAsync();
+
+            BuildWebHost(args).Run();
+            Console.WriteLine("Finished executing task!");
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+           WebHost
+           .CreateDefaultBuilder(args)
+           .UseStartup<Startup>()
+           .ConfigureAppConfiguration((builderContext, config) =>
+           {
+               config.AddCloudFoundry();
+           })
+           .ConfigureLogging((hostContext, logBuilder) =>
+           {
+               logBuilder.ClearProviders();
+               logBuilder.AddConfiguration(hostContext.Configuration.GetSection("Logging"));
+               logBuilder.AddDynamicConsole();
+               if (hostContext.HostingEnvironment.IsDevelopment())
+               {
+                   logBuilder.AddDebug();
+               }
+           }).Build();
+    }
+}
